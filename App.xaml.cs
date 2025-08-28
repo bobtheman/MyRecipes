03f0aff0914f1caa7bc8b81c78b06cd19e6bf184
AzureAD\RobertOwen <r.owen@accredit-solutions.com>
2025-08-28 11:51:51 +0100

    update

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,7 +9,7 @@
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new MainPage()) { Title = "MyRecipes" };
+            return new Window(new MainPage()) { Title = "My_Recipes" };
         }
     }
 }
